eead5698fb10f292feea43317b3dbeb6b6b648ff
klxcoder <klxcoder@gmail.com>
2024-12-27 14:44:33 +0700

    table with 32 columns

--- src/components/IP.tsx
+++ src/components/IP.tsx
@@ -2,9 +2,13 @@ import styles from './IP.module.scss'
 
 function IP() {
 
+  const bits: number[] = new Array(100).fill(0);
+
   return (
     <div className={styles.ip}>
-      IP
+      {bits.map(bit => (
+        <button className={styles.bit}>{bit}</button>
+      ))}
     </div>
   )
 }
