ff491464520eb519977debc00315220385bd368a
klxcoder <klxcoder@gmail.com>
2024-12-27 15:13:20 +0700

    can reverse bit when click

--- src/components/IP.tsx
+++ src/components/IP.tsx
@@ -1,26 +1,42 @@
-import { useState } from 'react';
+import { useState } from 'react'
 import styles from './IP.module.scss'
 
-const IP_VERSION = [0, 1, 0, 0];
+const IP_VERSION = [0, 1, 0, 0]
 
-const getBitsUI = (bits: number[], css: string) => bits.map(bit => (
-  <button className={styles[css]}>{bit}</button>
+const getBitsUI = (
+  bits: number[],
+  css: string,
+  onClick: (index: number) => void,
+) => bits.map((bit, index) => (
+  <button
+    className={styles[css]}
+    onClick={() => onClick(index)}
+  >{bit}</button>
 ))
+
+const getReverseFn = (
+  value: number[],
+  setValue: React.Dispatch<React.SetStateAction<number[]>>
+) => (index: number) => {
+  const newValue: number[] = [...value]
+  newValue[index] = newValue[index] === 0 ? 1 : 0
+  setValue(newValue)
+}
 
 function IP() {
   // Internet Header Length
-  const [ihl, setIhl] = useState<number[]>([0, 1, 1, 1]);
+  const [ihl, setIhl] = useState<number[]>([0, 1, 1, 1])
   // Type of Service
-  const [tos, setTos] = useState<number[]>(new Array(8).fill(0));
+  const [tos, setTos] = useState<number[]>(new Array(8).fill(0))
   // All the bits
-  const [tl, setTl] = useState<number[]>(new Array(16).fill(0));
+  const [tl, setTl] = useState<number[]>(new Array(16).fill(0))
 
   return (
     <div className={styles.ip}>
-      {getBitsUI(IP_VERSION, 'version')}
-      {getBitsUI(ihl, 'ihl')}
-      {getBitsUI(tos, 'tos')}
-      {getBitsUI(tl, 'tl')}
+      {getBitsUI(IP_VERSION, 'version', () => { })}
+      {getBitsUI(ihl, 'ihl', getReverseFn(ihl, setIhl))}
+      {getBitsUI(tos, 'tos', getReverseFn(tos, setTos))}
+      {getBitsUI(tl, 'tl', getReverseFn(tl, setTl))}
     </div>
   )
 }
