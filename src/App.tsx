4e1240b5ccc0da120d42cdce5c7fa8538925ef67
klxcoder <klxcoder@gmail.com>
2024-12-27 14:39:21 +0700

    IP.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import styles from './App.module.scss'
+import IP from './components/IP'
 
 function App() {
 
   return (
     <div className={styles.app}>
-      minimal
+      <IP />
     </div>
   )
 }
---
+++ src/components/IP.tsx
@@ -0,0 +1,12 @@
+import styles from './IP.module.scss'
+
+function IP() {
+
+  return (
+    <div className={styles.ip}>
+      IP
+    </div>
+  )
+}
+
+export default IP
