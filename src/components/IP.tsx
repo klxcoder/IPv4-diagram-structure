4d1267a71e225f194f25beb93abd3225e0322eb3
klxcoder <klxcoder@gmail.com>
2024-12-27 14:58:39 +0700

    first line

--- src/components/IP.tsx
+++ src/components/IP.tsx
@@ -1,14 +1,26 @@
+import { useState } from 'react';
 import styles from './IP.module.scss'
 
-function IP() {
+const IP_VERSION = [0, 1, 0, 0];
 
-  const bits: number[] = new Array(100).fill(0);
+const getBitsUI = (bits: number[], css: string) => bits.map(bit => (
+  <button className={styles[css]}>{bit}</button>
+))
+
+function IP() {
+  // Internet Header Length
+  const [ihl, setIhl] = useState<number[]>([0, 1, 1, 1]);
+  // Type of Service
+  const [tos, setTos] = useState<number[]>(new Array(8).fill(0));
+  // All the bits
+  const [tl, setTl] = useState<number[]>(new Array(16).fill(0));
 
   return (
     <div className={styles.ip}>
-      {bits.map(bit => (
-        <button className={styles.bit}>{bit}</button>
-      ))}
+      {getBitsUI(IP_VERSION, 'version')}
+      {getBitsUI(ihl, 'ihl')}
+      {getBitsUI(tos, 'tos')}
+      {getBitsUI(tl, 'tl')}
     </div>
   )
 }
